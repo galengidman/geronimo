95b25ea4638d502428b9856c4ed5308b4ee0d6c6
Galen Gidman <galen@galengidman.com>
2015-12-03 16:43:03 -0600

    hello code

---
+++ jquery.geronimo.js
@@ -0,0 +1,73 @@
+( function( $ ) {
+
+  $.fn.geronimo = function() {
+
+    $els = this;
+
+    var calcOffset = function( $parent ) {
+      var sizes        = getSizes( $parent );
+      var compensation = 16.667;
+
+      var totalInViewport   = sizes.windowHeight + sizes.parentHeight;
+      var inViewport        = ( sizes.scrollPos + sizes.windowHeight ) - sizes.parentOffset;
+      var percentInViewport = ( inViewport / totalInViewport ) * 100;
+
+      var offset = ( ( percentInViewport / 100 ) * ( compensation * 2 ) ) - compensation;
+
+      return offset;
+    };
+
+    var getSizes = function( $parent ) {
+      return {
+        parentHeight : $parent[0].offsetHeight,
+        parentOffset : $parent.offset().top,
+        parentTop    : $parent[0].getBoundingClientRect().top,
+        scrollPos    : window.scrollY,
+        windowHeight : window.innerHeight
+      }
+    };
+
+    var inViewport = function( $parent ) {
+      var sizes = getSizes( $parent );
+      return sizes.windowHeight >= sizes.parentTop && ( sizes.parentTop + sizes.parentHeight ) >= 0;
+    };
+
+    var position = function( $parallax, $parent ) {
+      if ( ! inViewport( $parent ) ) return;
+      $parallax.css( 'transform', 'translateY(' + calcOffset( $parent ) + '%)' );
+    };
+
+    var geronimo = function() {
+
+      $els.each( function() {
+        var $this     = $( this );
+        var $parallax = $this;
+        var $parent   = $this.parent();
+
+        position( $parallax, $parent );
+
+        $parent.css( {
+          'overflow' : 'hidden',
+          'position' : 'relative',
+        } );
+
+        $parallax.css( {
+          'bottom'   : '-25%',
+          'left'     : '0',
+          'position' : 'absolute',
+          'right'    : '0',
+          'top'      : '-25%'
+        } );
+      } );
+
+    };
+
+    geronimo();
+
+    $( window ).on( 'scroll resize', function() {
+      window.requestAnimationFrame( geronimo );
+    } );
+
+  };
+
+}( jQuery ) );
